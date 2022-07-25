c20ba00a6ab31084167d1aba53a88ade817b4a91
Roberto Monplaisir <castedcrown84@gmail.com>
2022-07-25 19:26:33 -0400

    added content to homepage and nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import Home from "./components/home";
+import Navbar from "./components/navbar";
 
 
 const App = () => {
   return ( 
 
     <div className="App">
+        <Navbar/>
       <div className="content">
+        <Home/>
 
       </div>
     </div>
---
+++ src/components/navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <nav>
+            <h1>Ninja Dojo</h1>
+
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">Create New</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
