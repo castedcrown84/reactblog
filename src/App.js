431e869bd481901df16594b886474da3d5e81168
Roberto Monplaisir <castedcrown84@gmail.com>
2022-07-25 19:01:45 -0400

    1st commit of React Blog

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const App = () => {
+  return ( 
+
+    <div className="App">
+      <div className="content">
+
+      </div>
+    </div>
+
+   );
+}
+ 
+export default App;
