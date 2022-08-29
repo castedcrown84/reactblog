17112aed929dd8000aa0b6a9f6bdf021f9cc3946
Roberto Monplaisir <castedcrown84@gmail.com>
2022-08-29 09:03:31 -0400

    added react router tenets

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from "react";
 import Home from "./components/home";
 import Navbar from "./components/navbar";
-
+import Create from "./components/create";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 
 const App = () => {
   return ( 
-
+    <Router>
     <div className="App">
         <Navbar/>
       <div className="content">
-        <Home/>
-
+        <Switch>
+          <Route exact path={"/"}>
+            <Home/>
+          </Route>
+          <Route path={"/create"}>
+            <Create/>
+          </Route>
+        </Switch>
       </div>
     </div>
+    </Router>
 
    );
 }
--- src/components/home.js
+++ src/components/home.js
@@ -1,40 +1,14 @@
-import { useState, useEffect } from "react";
-
 import BlogList from "./bloglist";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null)
-  const [isPending, setIsPending] = useState(true)
-  const [error, setError] = useState(null)
-
-  useEffect(() => {
-
-    fetch(`http://localhost:8000/blogs`)
-    .then((res) => {
-      if(!res.ok){
-        throw Error("could not fetch the data for that resource")
-      }
-      return res.json()
-    })
-    .then((data) => {
-      setBlogs(data)
-      setIsPending(false)
-      setError(null)
-    }).catch((err) => {
-        
-        setError(err.message)
-    })
-
-
-  }, [])
-
   
-
+  const {data:blogs, ispending, error} = useFetch(`http://localhost:8000/blogs`)
 
   return (
     <div className="home">
       {error && <div>{error}</div>}
-      {isPending && <div>Is Loading..</div>}
+      {ispending && <div>Is Loading..</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
     </div>
   );
