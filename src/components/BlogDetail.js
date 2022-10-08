ddbf0135cff991339be57f48ec372271ab1ee9c1
Roberto Monplaisir <castedcrown84@gmail.com>
2022-10-08 10:50:44 -0400

    added form submit and button fuctionality to make posts request

--- src/components/BlogDetail.js
+++ src/components/BlogDetail.js
@@ -1,14 +1,25 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetail = () => {
     
     const {id} = useParams()
+    const {data:blog, isPending, error} = useFetch('http://localhost:8000/blogs/' + id)
     
     
     return(
 
-    <div className="blog-detai">
-        <h2>Blog Details - {id}</h2>
+    <div className="blog-detail">
+        {isPending && <div>is pending....</div>}
+        {error && <div>{error}</div>}
+        {blog && (
+            <article>
+                <h2>{blog.title}</h2>
+                <p>Written by {blog.author}</p>
+                <div>{blog.body}</div>
+            </article>
+
+        )}
     </div>
     
     );
